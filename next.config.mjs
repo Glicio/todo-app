6b636752812330bac3c0589933d3e8f39bb58aab
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-05-21 21:54:04 -0300

    added img sources to next config

--- next.config.mjs
+++ next.config.mjs
@@ -18,5 +18,8 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+    images: {
+        domains: ['lh3.googleusercontent.com', 'cdn.discordapp.com'],
+    }
 };
 export default config;
