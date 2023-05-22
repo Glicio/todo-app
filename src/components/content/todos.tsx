29dd00a19d0ebd4126f1f9890251da7eaf8253a1
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-05-21 22:44:02 -0300

    the main component

---
+++ src/components/content/todos.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export default function Todos() {
+    return (
+        <div>
+            <div className="flex gap-2">
+                <button className="primary-button">New to-do</button>
+                <button className="primary-button">New category</button>
+            </div>
+            <h1>Todos</h1>
+        </div>
+    )
+}
+
