c504b651efcdddeb85aeb0c3e74374eb8349fe2e
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-05-29 22:40:40 -0300

    added planet scale env variables

--- src/env.mjs
+++ src/env.mjs
@@ -5,7 +5,11 @@ import { z } from "zod";
  * built with invalid env vars.
  */
 const server = z.object({
+    //DATABASE
+    // PRISMA
     DATABASE_URL: z.string().url(),
+    // PLANETSCALE
+
     NODE_ENV: z.enum(["development", "test", "production"]),
     NEXTAUTH_SECRET:
         process.env.NODE_ENV === "production"
@@ -24,6 +28,9 @@ const server = z.object({
     //GOOGLE
     GOOGLE_CLIENT_ID: z.string(),
     GOOGLE_CLIENT_SECRET: z.string(),
+    DATABASE_HOST: z.string(),
+    DATABASE_USERNAME: z.string(),
+    DATABASE_PASSWORD: z.string(),
 });
 
 /**
@@ -51,6 +58,10 @@ const processEnv = {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+    // PLANETSCALE
+    DATABASE_HOST: process.env.DATABASE_HOST,
+    DATABASE_USERNAME: process.env.DATABASE_USERNAME,
+    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
 };
 
 // Don't touch the part below
