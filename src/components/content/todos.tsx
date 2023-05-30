0c697e930574edc88c5a2b85c823e3bd0b829e53
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-05-29 22:41:14 -0300

    now can list todos

--- src/components/content/todos.tsx
+++ src/components/content/todos.tsx
@@ -1,15 +1,35 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { api } from "~/utils/api";
 
 export default function Todos() {
+    const todos = api.todos.getUserTodos.useQuery({ excludeDone: false });
+
+    useEffect(() => {
+        console.log(todos.data);
+    }, [todos.data]);
+
     return (
         <div>
-            <div className="flex gap-2">
+            <div className="flex justify-center gap-2 p-2">
                 <button className="primary-button">New to-do</button>
                 <button className="primary-button">New category</button>
             </div>
-            <h1>Todos</h1>
+            {todos.data?.categories &&
+                todos.data.categories.map((category) => (
+                    <div key={category.id}>
+                        <p className="bg-red-500">{category.name}</p>
+                        {todos.data?.todos &&
+                            todos.data.todos
+                                .filter(
+                                    (todo) => todo.categoryId === category.id
+                                )
+                                .map((todo) => (
+                                    <div key={todo.id}>
+                                        <p>{todo.title}</p>
+                                    </div>
+                                ))}
+                    </div>
+                ))}
         </div>
-    )
+    );
 }
-
