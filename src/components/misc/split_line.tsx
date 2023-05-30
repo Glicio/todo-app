11d39ab01d916e4ec389e985b30ad34c4bdf0d3d
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-05-30 20:43:34 -0300

    a simple component to use between sections

---
+++ src/components/misc/split_line.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SplitLine() {
+    return <div className="my-2 w-full h-[1px] bg-gradient-to-r from-transparent via-gray-400 to-transparent "></div>;
+}
