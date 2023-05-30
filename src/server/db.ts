c2204665758eb10cac8622cace0233bf4d1f1e29
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-05-29 22:40:15 -0300

    added db.js instance

--- src/server/db.ts
+++ src/server/db.ts
@@ -2,9 +2,19 @@ import { PrismaClient } from "@prisma/client";
 
 import { env } from "~/env.mjs";
 
+import { connect } from "@planetscale/database";
+
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
+
+const psConfig = {
+    host: env.DATABASE_HOST,
+    username: env.DATABASE_USERNAME,
+    password: env.DATABASE_PASSWORD,
+}
+
+export const db = connect(psConfig); 
 
 export const prisma =
   globalForPrisma.prisma ??
