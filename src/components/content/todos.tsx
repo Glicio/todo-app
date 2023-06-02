8e92386e1e8abcebf0d569a2530e81b3409cf894
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-06-02 19:56:16 -0300

    minor things

--- src/components/content/todos.tsx
+++ src/components/content/todos.tsx
@@ -30,55 +30,67 @@ export default function Todos() {
         );
     return (
         <div>
-            {showAddTodo ? <AddTodo close={() => setShowAddTodo(false)} /> : ""}
+            <AddTodo close={() => setShowAddTodo(false)} opened={showAddTodo}/> 
             {!showAddTodo && (
                 <button
                     onClick={() => setShowAddTodo(true)}
-                    className="fixed bottom-20 w-12 h-12 right-4 z-10 rounded-full bg-[var(--secondary-color)] text-gray-300"
+                    className="fixed bottom-20 right-4 z-10 h-12 w-12 rounded-full bg-[var(--secondary-color)] text-gray-300"
                 >
                     <AddIcon />
                 </button>
             )}
             <div className="flex flex-col gap-2 p-2 ">
-                <div>
-                    <div className="flex items-center gap-1">
-                        <span>Categories</span>
-                        {selectedCategory.length > 0 ? (
-                            <button className="h-4 w-4 text-xs" onClick={() => {setSelectedCategory([])}}>
-                                <CloseIcon />
-                            </button>
-                        ) : null}
-                    </div>
-                    <div className="thin-scroll flex gap-2 overflow-auto pb-2 pt-1">
-                        {todos?.categories &&
-                            todos.categories.map((category) => (
-                                <CategoryLabel
-                                    active={selectedCategory.includes(
-                                        category.id
-                                    )}
-                                    onClick={(categoryId) => {
-                                        if (
-                                            selectedCategory.includes(
+                {todos?.categories && todos.categories.length > 0 ? (
+                    <>
+                        <div>
+                            <div className="flex items-center gap-1">
+                                <span>Categories</span>
+                                {selectedCategory.length > 0 ? (
+                                    <button
+                                        className="h-4 w-4 text-xs"
+                                        onClick={() => {
+                                            setSelectedCategory([]);
+                                        }}
+                                    >
+                                        <CloseIcon />
+                                    </button>
+                                ) : null}
+                            </div>
+
+                            <div className="thin-scroll flex gap-2 overflow-auto pb-2 pt-1">
+                                {todos?.categories &&
+                                    todos.categories.map((category) => (
+                                        <CategoryLabel
+                                            active={selectedCategory.includes(
                                                 category.id
-                                            )
-                                        )
-                                            return setSelectedCategory(
-                                                selectedCategory.filter(
-                                                    (id) => id !== categoryId
+                                            )}
+                                            onClick={(categoryId) => {
+                                                if (
+                                                    selectedCategory.includes(
+                                                        category.id
+                                                    )
                                                 )
-                                            );
-                                        setSelectedCategory((old) => [
-                                            ...old,
-                                            categoryId,
-                                        ]);
-                                    }}
-                                    key={category.id}
-                                    category={category}
-                                />
-                            ))}
-                    </div>
-                </div>
-                <div className="my-2 border-b border-[var(--tertiary-color)]"></div>
+                                                    return setSelectedCategory(
+                                                        selectedCategory.filter(
+                                                            (id) =>
+                                                                id !==
+                                                                categoryId
+                                                        )
+                                                    );
+                                                setSelectedCategory((old) => [
+                                                    ...old,
+                                                    categoryId,
+                                                ]);
+                                            }}
+                                            key={category.id}
+                                            category={category}
+                                        />
+                                    ))}
+                            </div>
+                        </div>
+                        <div className="my-2 border-b border-[var(--tertiary-color)]"></div>{" "}
+                    </>
+                ) : null}
                 {todos?.todos &&
                     todos.todos
                         .filter((todo) => {
@@ -86,6 +98,7 @@ export default function Todos() {
                             return selectedCategory.includes(todo.categoryId);
                         })
                         .map((todo) => <Todo key={todo.id} todo={todo} />)}
+                        <div className="h-14 w-full"></div>
             </div>
         </div>
     );
