cd44837480fee80592acabcaeba8911d067f01f0
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-06-03 19:29:54 -0300

    made the color property otional

--- src/components/misc/loading_icon.tsx
+++ src/components/misc/loading_icon.tsx
@@ -2,7 +2,7 @@ import { Loader } from "@mantine/core";
 import React from "react";
 
 
-export default function LoadingIcon({color}: {color: string}) {
+export default function LoadingIcon({color}: {color?: string}) {
     return (
         <Loader color={color ? color : "var(--tertiary-color)"} size="sm"/>
     )
