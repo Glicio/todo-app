3ac350db68a6cd390f1c8d94f9779f219430e11b
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-06-02 22:43:33 -0300

    added teams

--- src/server/api/routers/teams.ts
+++ src/server/api/routers/teams.ts
@@ -7,13 +7,39 @@ import { db, prisma } from "~/server/db";
 
 
 export const teams = createTRPCRouter({
-    
+    /**
+    * returns the list of teams for the current user
+    * */
+    getUserTeams: protectedProcedure.query(async ({ ctx }) => {
+        try {
+        const teams = await prisma.team.findMany({
+            where: {
+                users: {
+                    some: {
+                        id: ctx.session.user.id,
+                    }
+                }
+            }
+        });
+        return teams;
+        } catch (e) {
+            console.log(e);
+            throw new TRPCError({
+                code: "INTERNAL_SERVER_ERROR",
+                message: "Error while fetching teams",
+            });
+        }
+    }),
+    /**
+    * creates a team for the current user
+    * */
     createTeam: protectedProcedure.input(
         z.object({
             name: z.string(),
+            color: z.string().regex(/^#[0-9A-F]{6}$/i),
         })).mutation(async ({input, ctx}) => {
         
-            const userTeamsCountSmt = `SELECT COUNT(*) FROM teams WHERE owner_id = ?`;
+            const userTeamsCountSmt = `SELECT COUNT(*) FROM Team WHERE ownerId = ?`;
             const userTeamsCount = await db.execute(userTeamsCountSmt, [ctx.session.user.id]);
             if(!userTeamsCount.rows[0]) {
                 throw new TRPCError({
@@ -22,12 +48,13 @@ export const teams = createTRPCRouter({
                 });
             }
             if (userTeamsCount.rows[0]['COUNT(*)' as keyof typeof userTeamsCount.rows[0]] >= 3) {
-                throw new Error('You can only create 3 teams');
+                throw new TRPCError({code: "BAD_REQUEST", message: 'You can only create 3 teams'});
             }
 
             const team = await prisma.team.create({
                 data: {
                     name: input.name,
+                    color: input.color,
                     users: {
                         connect: {
                             id: ctx.session.user.id,
