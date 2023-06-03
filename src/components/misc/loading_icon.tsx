38f706232133cdfc2a0c72487b15d712eb9fddc2
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-06-02 22:42:38 -0300

    a small loading icon

---
+++ src/components/misc/loading_icon.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+export default function LoadingIcon() {
+    return (
+        <div className="w-8 h-8 border-b rounded-full animate-spin">
+        </div>
+        )
+}
