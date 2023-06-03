8243632787d5a8f50f72a9ce5ef830a3ce126a83
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-06-02 23:24:39 -0300

    changed to the agent model

--- src/server/api/routers/category.ts
+++ src/server/api/routers/category.ts
@@ -1,19 +1,15 @@
 import { z } from "zod";
 import { TRPCError } from "@trpc/server";
 
-import {
-    createTRPCRouter,
-    protectedProcedure,
-} from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { db } from "~/server/db";
 import type { CategoryResponse } from "~/utils/db_responses";
-
-
+import checkUserInTeam from "~/utils/check_usr_in_team";
 
 export const category = createTRPCRouter({
     /**
-    * returns the count of categories for the current user
-    * */
+     * returns the count of categories for the current user
+     * */
     getCategoryCount: protectedProcedure.query(async ({ ctx }) => {
         if (!ctx.session.user) throw new TRPCError({ code: "UNAUTHORIZED" });
         const smt = `SELECT COUNT(*) FROM Category WHERE userId = ?;`;
@@ -21,9 +17,9 @@ export const category = createTRPCRouter({
         try {
             const query = await db.execute(smt, [ctx.session.user.id]);
             const rows = query.rows as count[];
-            if(!query || !rows[0]) return 0
-            const count = rows[0]["count(*)"] ? rows[0]["count(*)"] : 0
-            return count
+            if (!query || !rows[0]) return 0;
+            const count = rows[0]["count(*)"] ? rows[0]["count(*)"] : 0;
+            return count;
         } catch (e) {
             console.log(e);
             throw new TRPCError({
@@ -33,16 +29,34 @@ export const category = createTRPCRouter({
         }
     }),
     /**
-    * returns a list of categories for the current user
-    * */
-    getUserCategories: protectedProcedure
-        .query(async ({ ctx }) => {
-            const smt = `SELECT * FROM Category WHERE userId = ?;`;
+     * returns a list of categories for the given agent
+     * */
+    getAgentCategories: protectedProcedure
+        .input(
+            z.object({
+                agentType: z.enum(["user", "team"]),
+                agentId: z.string(),
+            })
+        )
+        .query(async ({ ctx, input }) => {
+            const { agentType, agentId } = input;
+
+            if (
+                agentType === "team" &&
+                !(await checkUserInTeam({
+                    teamId: agentId,
+                    userId: ctx.session.user.id,
+                }))
+            ) {
+                throw new TRPCError({ code: "UNAUTHORIZED" });
+            }
+            const column = agentType === "user" ? "userId" : "teamId";
+            const smt = `SELECT * FROM Category WHERE ${column} = ?;`;
             try {
                 const query = await db.execute(smt, [ctx.session.user.id]);
                 const rows = query.rows as CategoryResponse[];
                 if (!query || !rows[0]) return [];
-                return rows
+                return rows;
             } catch (e) {
                 console.log(e);
                 throw new TRPCError({
@@ -50,6 +64,5 @@ export const category = createTRPCRouter({
                     message: "Error while fetching categories",
                 });
             }
-        })
-    });
-
+        }),
+});
