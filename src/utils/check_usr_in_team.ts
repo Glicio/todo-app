cab84cb0cd5d09adf2cfbdeee12b31c7e712736a
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-06-03 23:33:36 -0300

    fixed the way I get the count from a SELECT count(*), this may blow up later idk

--- src/utils/check_usr_in_team.ts
+++ src/utils/check_usr_in_team.ts
@@ -1,16 +1,21 @@
-import { prisma } from "../server/db";
+import { db, prisma } from "../server/db";
+import getCountFromDBQuery from "./getCountFromDBQuery";
 
 export default async function checkUserInTeam({ teamId, userId }: { teamId: string, userId: string }) {
-    const team = await prisma.team.findMany({
-        where: {    
-            id: teamId,
-            users: {
-                some: {
-                    id: userId,
-                },
-            },
-        },
-    });
-    if (!team) return false
+    const smt = "SELECT count(*) FROM _TeamToUser WHERE A = ? AND B = ?"
+    const res = await db.execute(smt, [teamId, userId])
+    const count = getCountFromDBQuery(res.rows)
+//   const team = await prisma.team.findMany({
+//       where: {    
+//           id: teamId,
+//           users: {
+//               some: {
+//                   id: userId,
+//               },
+//           },
+//       },
+//   });
+    console.log("relation count", count)
+    if (count !== 1) return false
     return true
 }
--- src/utils/getCountFromDBQuery.ts
+++ src/utils/getCountFromDBQuery.ts
@@ -3,6 +3,6 @@ export default function getCountFromDBQuery(rows: Record<string, any>[]) {
     if (!rows || !rows[0]) {
     throw new Error("No rows returned from query");
     }
-  return rows[0]["count(*)" as keyof typeof rows[0]] as number;
+  return Number(rows[0]["count(*)" as keyof typeof rows[0]]);
 }
 
