1249e1ab4b8e6fa9e23b5a31116836b5ac23cbc1
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-06-03 23:09:29 -0300

    added the categories page to navigation

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -39,7 +39,7 @@ export default function Navigation() {
                     title="Categories"
                     icon={<TagIcon />}
                     onClick={() => {
-                        alert("cat");
+                        void router.push("/categories");
                     }}
                 />
                 <NavButton
