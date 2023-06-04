b31a358412b61988ff79d2b05b0ced204536eb05
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-06-03 23:45:44 -0300

    undone some bs I did

--- src/utils/check_usr_in_team.ts
+++ src/utils/check_usr_in_team.ts
@@ -1,20 +1,16 @@
-import { db, prisma } from "../server/db";
-import getCountFromDBQuery from "./getCountFromDBQuery";
+import { prisma } from "../server/db";
 
 export default async function checkUserInTeam({ teamId, userId }: { teamId: string, userId: string }) {
-    const smt = "SELECT count(*) FROM _TeamToUser WHERE A = ? AND B = ?"
-    const res = await db.execute(smt, [teamId, userId])
-    const count = getCountFromDBQuery(res.rows)
-//   const team = await prisma.team.findMany({
-//       where: {    
-//           id: teamId,
-//           users: {
-//               some: {
-//                   id: userId,
-//               },
-//           },
-//       },
-//   });
-    if (count !== 1) return false
+    const team = await prisma.team.findMany({
+        where: {    
+            id: teamId,
+            users: {
+                some: {
+                    id: userId,
+                },
+            },
+        },
+    });
+    if (!team) return false
     return true
 }
