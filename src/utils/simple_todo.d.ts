658a74ac467dcdf011460f1e25b5bb50b33d8bbf
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-06-04 23:24:21 -0300

    create some helper types

---
+++ src/utils/simple_todo.d.ts
@@ -0,0 +1,10 @@
+import type { Todo } from "@prisma/client";
+import type SimpleUser from "./simple_user";
+import type SimpleCategory from "./simple_category";
+
+export default interface SimpleTodo extends Todo {
+    category:  SimpleCategory | null | undefined;
+    createdBy: SimpleUser, 
+    updatedBy: SimpleUser | null;
+    assignedTo: SimpleUser | null;
+}
