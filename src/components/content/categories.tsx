5b28216029f2191856fd25b7cb58a2f417c17861
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-06-06 20:58:03 -0300

    added loading screen to the categories component

--- src/components/content/categories.tsx
+++ src/components/content/categories.tsx
@@ -43,6 +43,12 @@ export default function Categories() {
         ));
     }
 
+    if (categoriesMutation.isLoading)
+        return (
+            <div className="flex h-screen items-center justify-center">
+                <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-gray-900"></div>
+            </div>
+        );
 
     return (
         <div>
