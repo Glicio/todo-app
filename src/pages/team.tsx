690145f463ebc4177cde5e4444fed5d12a01e13a
Glicio <glicioo@outlook.com>
2023-06-10 16:36:49 -0300

    removed unused import

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -2,7 +2,6 @@
 import { type NextPage } from "next";
 import { useSession } from "next-auth/react";
 import Teams from "~/components/content/teams";
-import Todos from "~/components/content/todos";
 import MainLayout from "~/components/layouts/main_layout";
 
 const Team: NextPage = () => {
