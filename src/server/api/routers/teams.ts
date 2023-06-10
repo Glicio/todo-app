beed7c2b456df8484fbbbcd284d6b4c77400c316
Glicio <glicioo@outlook.com>
2023-06-10 14:52:19 -0300

    added get user and update team procedure

--- src/server/api/routers/teams.ts
+++ src/server/api/routers/teams.ts
@@ -1,7 +1,8 @@
 import { TRPCError } from "@trpc/server";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { z } from "zod";
-import { db, prisma } from "~/server/db";
+import { prisma } from "~/server/db";
+import checkUserInTeam from "~/utils/check_usr_in_team";
 
 
 
@@ -12,16 +13,16 @@ export const teams = createTRPCRouter({
     * */
     getUserTeams: protectedProcedure.query(async ({ ctx }) => {
         try {
-        const teams = await prisma.team.findMany({
-            where: {
-                users: {
-                    some: {
-                        id: ctx.session.user.id,
+            const teams = await prisma.team.findMany({
+                where: {
+                    users: {
+                        some: {
+                            id: ctx.session.user.id,
+                        }
                     }
                 }
-            }
-        });
-        return teams;
+            });
+            return teams;
         } catch (e) {
             console.log(e);
             throw new TRPCError({
@@ -37,8 +38,8 @@ export const teams = createTRPCRouter({
         z.object({
             name: z.string().min(3).max(20),
             color: z.string().regex(/^#[0-9A-F]{6}$/i),
-        })).mutation(async ({input, ctx}) => {
-        
+        })).mutation(async ({ input, ctx }) => {
+
             const user = await prisma.user.findUnique({
                 where: {
                     id: ctx.session.user.id,
@@ -47,13 +48,13 @@ export const teams = createTRPCRouter({
                     teamsCreatedCount: true,
                 },
             });
-            if(!user) {
-                throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: 'User not found'});
+            if (!user) {
+                throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", message: 'User not found' });
             }
             if (user.teamsCreatedCount >= 3) {
-                throw new TRPCError({code: "BAD_REQUEST", message: 'You can only create 3 teams'});
+                throw new TRPCError({ code: "BAD_REQUEST", message: 'You can only create 3 teams' });
             }
-            
+
             const newTeam = prisma.team.create({
                 data: {
                     name: input.name,
@@ -81,8 +82,8 @@ export const teams = createTRPCRouter({
                 }
             });
             try {
-            const [team] = await prisma.$transaction([newTeam, userUpdate]);
-            return team;
+                const [team] = await prisma.$transaction([newTeam, userUpdate]);
+                return team;
             } catch (e) {
                 console.log(e);
                 throw new TRPCError({
@@ -91,4 +92,83 @@ export const teams = createTRPCRouter({
                 });
             }
         }),
+    /**
+     * returns the list of users for a given team
+     * */
+    getTeamUsers: protectedProcedure.input(
+        z.object({
+            teamId: z.string(),
+        })).query(async ({ input, ctx }) => {
+            if (!await checkUserInTeam({ userId: ctx.session.user.id, teamId: input.teamId })) {
+                throw new TRPCError({
+                    code: "BAD_REQUEST",
+                    message: "You are not in this team",
+                });
+            }
+            const team = await prisma.team.findUnique({
+                where: {
+                    id: input.teamId,
+                },
+                include: {
+                    users: true,
+                },
+            });
+            if (!team) {
+                throw new TRPCError({
+                    code: "BAD_REQUEST",
+                    message: "Team not found",
+                });
+            }
+            return team.users;
+        }
+        ),
+    /**
+     * update team
+     * */
+    updateTeam: protectedProcedure.input(
+        z.object({
+            teamId: z.string(),
+            name: z.string().min(3).max(20),
+            color: z.string().regex(/^#[0-9A-F]{6}$/i),
+        })).mutation(async ({ input, ctx }) => {
+            if (!await checkUserInTeam({ userId: ctx.session.user.id, teamId: input.teamId })) {
+                throw new TRPCError({
+                    code: "BAD_REQUEST",
+                    message: "You are not in this team",
+                });
+            }
+            const oldTeam = await prisma.team.findUnique({
+                where: {
+                    id: input.teamId,
+                },
+                select: {
+                    ownerId: true,
+                },
+            });
+            
+            if (!oldTeam || oldTeam.ownerId !== ctx.session.user.id) {
+                throw new TRPCError({
+                    code: "BAD_REQUEST",
+                    message: "You are not the owner of this team",
+                });
+            }
+
+            const team = await prisma.team.update({
+                where: {
+                    id: input.teamId,
+                },
+                data: {
+                    name: input.name,
+                    color: input.color,
+                },
+            });
+            if (!team) {
+                throw new TRPCError({
+                    code: "BAD_REQUEST",
+                    message: "Team not found",
+                });
+            }
+            return team;
+        }
+        ),
 });
