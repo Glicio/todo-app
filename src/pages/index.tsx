c6a9168c02b76b0e3b9d1ae23418d5ef31e3845d
Glicio <glicioo@outlook.com>
2023-06-16 22:55:51 -0300

    added the teams tab

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,20 @@
 import { Tabs } from "@mantine/core";
 import { type NextPage } from "next";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useContext } from "react";
 import Categories from "~/components/content/categories";
 import UserNotAuthed from "~/components/content/not_authed";
+import Teams from "~/components/content/teams";
 import Todos from "~/components/content/todos";
 import MainLayout from "~/components/layouts/main_layout";
+import { userContext } from "~/contexts/UserProvider";
 
 const Home: NextPage = () => {
 
     const { data: session } = useSession();
-
+    const { agentType } = useContext(userContext)
+    const router = useRouter();
     if (!session || !session.user) {
 
         return (
@@ -28,6 +33,9 @@ const Home: NextPage = () => {
                     }}
                     onTabChange={(value) => {
                         console.log(value)
+                        if (value === "team") {
+//                            void router.push("/team")
+                        }
                     }}
                 >
                     <div className="relative h-[2.25rem] w-full">
@@ -37,6 +45,10 @@ const Home: NextPage = () => {
                             <Tabs.Tab value="todos">Todos</Tabs.Tab>
                             <Tabs.Tab value="done">Done</Tabs.Tab>
                             <Tabs.Tab value="categories">Categories</Tabs.Tab>
+
+                            {agentType === "team" ? (
+                                <Tabs.Tab value="team">Team</Tabs.Tab>
+                            ) : null}
                         </Tabs.List>
                     </div>
                     <Tabs.Panel value="todos">
@@ -48,6 +60,11 @@ const Home: NextPage = () => {
                     <Tabs.Panel value="categories">
                         <Categories />
                     </Tabs.Panel>
+                    {agentType === "team" ? (
+                        <Tabs.Panel value="team">
+                            <Teams />
+                        </Tabs.Panel>
+                    ) : null}
 
 
                 </Tabs>
