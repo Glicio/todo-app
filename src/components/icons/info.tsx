b58753b606f1e65c522ca73c1eb86755ac99f31c
Glicio <glicioo@outlook.com>
2023-06-18 18:25:43 -0300

    fixed type of sizer property (thanks copilot...)

--- src/components/icons/info.tsx
+++ src/components/icons/info.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 
-export default function InfoIcon({ size }: { size?: number }) {
+export default function InfoIcon({ size }: { size?: string}) {
 	return (
 		<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
 			className={size ? `w-[${size}] h-[${size}]` : "w-6 h-6"}>
