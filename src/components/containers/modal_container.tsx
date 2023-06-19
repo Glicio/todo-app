52cfef0a95f3d96e448563088d70548c42ffb86c
Glicio <glicioo@outlook.com>
2023-06-19 02:50:24 -0300

    added color property

--- src/components/containers/modal_container.tsx
+++ src/components/containers/modal_container.tsx
@@ -7,23 +7,31 @@ export default function ModalContainer({
     children,
     title,
     keepMounted,
+    color,
 }: {
     opened: boolean;
     onClose: () => void;
     children: React.ReactNode;
     title?: string;
     keepMounted?: boolean;
+    color?: string;
 }) {
     return (
         <Modal opened={opened} onClose={onClose}
             title={title}
             keepMounted={keepMounted}
+            styles={{
+                content: {
+                    borderColor: color || "var(--secondary-color)",
+                }
+            }}
             classNames={{
                 overlay: "backdrop-blur",
                 header: "bg-[var(--primary-color)] ",
                 body: "bg-[var(--primary-color)] ",
-                content: "bg-[var(--primary-color)] w-fit border border-[var(--secondary-color)] rounded-b-md m-auto"
+                content: "bg-[var(--primary-color)] w-fit border rounded-b-md m-auto"
             }}>
+        
             {children}
         </Modal>
     );
