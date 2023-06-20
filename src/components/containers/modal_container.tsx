0d07038ff997a6f372157ceb824a10f10185c8d9
Glicio <glicioo@outlook.com>
2023-06-19 21:45:06 -0300

    changed the way the "border" works

--- src/components/containers/modal_container.tsx
+++ src/components/containers/modal_container.tsx
@@ -14,25 +14,44 @@ export default function ModalContainer({
     children: React.ReactNode;
     title?: string;
     keepMounted?: boolean;
-    color?: string;
+    color?: string | string[];
 }) {
+
+    const getBgColor = () => {
+        if (typeof color === "string") {
+            return {
+                backgroundColor: color,
+            }
+        } else if (color && color?.length > 0) {
+            return {
+                background: `linear-gradient(${color?.join(", ")})`,
+            }
+        } else {
+            return {
+                backgroundColor: "var(--tertiary-color)",
+            }
+        }
+    }
     return (
         <Modal opened={opened} onClose={onClose}
             title={title}
             keepMounted={keepMounted}
             styles={{
                 content: {
-                    borderColor: color || "var(--secondary-color)",
+                    ...getBgColor(),
+                    padding: "2px",
                 }
             }}
             classNames={{
                 overlay: "backdrop-blur",
                 header: "bg-[var(--primary-color)] ",
                 body: "bg-[var(--primary-color)] ",
-                content: "bg-[var(--primary-color)] w-fit border rounded-b-md m-auto"
+                content: "w-fit rounded-b-md m-auto ",
             }}>
-        
-            {children}
+
+            <div className="max-h-[75vh] overflow-y-auto">
+                {children}
+            </div>
         </Modal>
     );
 }
