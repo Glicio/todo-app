d84099ad9b2a1519a0701a998a82b1826c403148
Glicio <glicioo@outlook.com>
2023-06-23 23:46:50 -0300

    added steps

--- src/utils/simple_todo.d.ts
+++ src/utils/simple_todo.d.ts
@@ -2,9 +2,16 @@ import type { Todo } from "@prisma/client";
 import type SimpleUser from "./simple_user";
 import type SimpleCategory from "./simple_category";
 
+export interface SimpleStep {
+    id?: string;
+    title: string;
+    done: boolean;
+}
+
 export default interface SimpleTodo extends Todo {
     categories:  SimpleCategory[];
     createdBy: SimpleUser, 
     updatedBy: SimpleUser | null;
     assignedTo: SimpleUser[] | null;
+    steps: SimpleStep[] | null;
 }
