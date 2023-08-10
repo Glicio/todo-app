95e5882230997e944c73d14e2a181311881d29c9
Glicio <glicioo@outlook.com>
2023-08-10 19:18:40 -0300

    size is optional

--- src/components/icons/trash.tsx
+++ src/components/icons/trash.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Trash({size}: { size: string }) {
+export default function Trash({size}: { size?: string }) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
